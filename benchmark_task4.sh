d19253db60b11c5709021d0d2770059e030f7043
YuL <vdfnxk@SANJIAOTIE.station>
2025-04-28 01:17:22 +0100

    random output

---
+++ benchmark_task4.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+EXEC=./radiator_gpu_dp
+N=15360
+M=15360
+P=1000
+
+declare -a Bx=(16 32 64 8)
+declare -a By=(16 8 4 32)
+
+echo "bx,by,Propagate_ms,Average_ms,Total_ms" > bench_dp.txt
+
+for bx in "${Bx[@]}"; do
+  for by in "${By[@]}"; do
+    if (( N % bx != 0 || M % by != 0 )); then
+      continue
+    fi
+
+    out=$($EXEC -c -t -n $N -m $M -p $P -bx $bx -by $by | \
+      awk '
+        /GPU propagate/ {prop=$3}
+        /GPU average/   {avg=$3}
+        END {
+          total = prop + avg
+          print prop "," avg "," total
+        }
+      ')
+    
+    echo "$bx,$by,$out" >> bench_dp.txt
+  done
+done
+
+echo "Done. Results in bench_dp.txt"
